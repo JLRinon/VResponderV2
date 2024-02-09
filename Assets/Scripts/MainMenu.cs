2ce8d37b3d283eb5342f75ffbd1e24346a5c094b
JLRinon <rinon191@gmail.com>
2024-02-10 01:48:44 +0800

    Main Menu Script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     //{FOR MAIN MENU --START--}
     public void StartSimulation()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync("LP1");
     }
     public void QuitSimulation()
     {
@@ -17,36 +17,83 @@ public class MainMenu : MonoBehaviour
     //{FOR MAIN MENU --END--}
 
 
+    //{FOR LANDING PAGE --START--}
+    public void StartPA()
+    {
+        SceneManager.LoadSceneAsync("PA1");
+    }
+
+    public void StartRapid()
+    {
+        SceneManager.LoadSceneAsync("Rapid1");
+    }
+
+    //{FOR LANDING PAGE --END--}
+
 
     //{FOR PATIENT ASSESSMENT --START--}
-    public void StartPatientassessment()
-    {
-        SceneManager.LoadSceneAsync(2);
-    }
     public void StartPatientassessment2()
     {
-        SceneManager.LoadSceneAsync(3);
+        SceneManager.LoadSceneAsync("PA2");
     }
     public void StartPatientassessment3and4()
     {
-        SceneManager.LoadSceneAsync(4);
+        SceneManager.LoadSceneAsync("PA3N4");
     }
     public void StartPatientassessment5()
     {
-        SceneManager.LoadSceneAsync(5);
+        SceneManager.LoadSceneAsync("PA5");
     }
     public void StartPatientassessment6()
     {
-        SceneManager.LoadSceneAsync(6);
+        SceneManager.LoadSceneAsync("PA6");
     }
+    public void StartPatientassessment7()
+    {
+        SceneManager.LoadSceneAsync("PA7");
+    }
+    public void StartPatientassessment8_1()
+    {
+        SceneManager.LoadSceneAsync("PA8.1");
+    }
+    public void StartPatientassessment9_2()
+    {
+        SceneManager.LoadSceneAsync("PA9.2");
+    }
+    public void StartPatientassessment10_3()
+    {
+        SceneManager.LoadSceneAsync("PA10.3");
+    }
+
+
     //{FOR PATIENT ASSESSMENT --END--}
 
 
 
     //{FOR RAPID EXTRICATION --START--}
-    public void StartRapidExtrication1()
+    public void StartRapid2()
     {
-        SceneManager.LoadSceneAsync(5);
+        SceneManager.LoadSceneAsync("Rapid2");
+    }
+    public void StartRapid3()
+    {
+        SceneManager.LoadSceneAsync("Rapid3");
+    }
+    public void StartRapid4()
+    {
+        SceneManager.LoadSceneAsync("Rapid4");
+    }
+    public void StartRapid5()
+    {
+        SceneManager.LoadSceneAsync("Rapid5");
+    }
+    public void StartRapid5_6()
+    {
+        SceneManager.LoadSceneAsync("Rapid5_6");
+    }
+    public void StartRapid7()
+    {
+        SceneManager.LoadSceneAsync("Rapid7");
     }
     //{FOR RAPID EXTRICATION --END--}
 }
